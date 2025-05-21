2b53bd0bbc9d2855ec412de0ece6504d7fb43e12
ARUNKUMAR3957 <arunkumartrt933@gmail.com>
2025-05-21 18:41:34 +0530

    Delete sensors.py

--- sensors.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def get_sensor_data():
-    return {
-        "distance": round(random.uniform(10, 100), 2),
-        "speed": round(random.uniform(0, 50), 2),
-        "status": "running"
-    }
